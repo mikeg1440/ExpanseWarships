971c6d55c49080ce18caa97fda5eade964580f42
Mike Gaudreau <gaudreaum01@gmail.com>
2022-02-17 07:31:21 -0500

    Create a file for holding data we might change often and use in multiple places

---
+++ scripts/utils/data.js
@@ -0,0 +1,13 @@
+
+const shipsData = [
+  ["Rocinante", "Pella", "Donnager"],
+  [
+    "https://static.wikia.nocookie.net/expanse/images/5/54/RociArtS4.png/revision/latest/scale-to-width-down/350?cb=20220113235011",
+    "https://static.wikia.nocookie.net/expanse/images/4/47/PellaConceptship.png/revision/latest/scale-to-width-down/350?cb=20201223193751",
+    "https://static.wikia.nocookie.net/expanse/images/8/8b/MCRN_Donnager_Render.jpg/revision/latest/top-crop/width/200/height/150?cb=20210614202656"
+  ],
+  [ 200, 300, 180 ],
+  [ 100, 90, 105 ]
+]
+
+exports.shipsData = shipsData;
