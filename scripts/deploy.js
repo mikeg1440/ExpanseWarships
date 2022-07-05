ca435a2d9c56c702c3ab3bb9c3b76d6f249157b4
mikeg1440 <gaudreaum01@gmail.com>
2022-07-05 17:15:04 -0400

    Update deploy script with latest changes to attack

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,30 +5,46 @@ const main = async () => {
   const gameContract = await gameContractFactory.deploy(...shipsData);
   
   await gameContract.deployed();
-  
   console.log(`[+] Contract Address: ${gameContract.address}`);
   
   let txn;
+  txn = await gameContract.mintShipNFT(2);
+  await txn.wait();
   
-  for (let i=1; i <= 3; i++){
-    const shipId = randomShipNumber();
+  let returnedTokenUri = await gameContract.tokenURI(1);
+  console.log(`[+] Token URI: ${returnedTokenUri}`);
   
-    txn = await gameContract.mintShipNFT(shipId);
-    await txn.wait();
+  txn = await gameContract.attackBoss();
+  let resp = await txn.wait();
+  
+  txn = await gameContract.attackBoss();
+  resp = await txn.wait();
+  
+  // await gameContract.deployed();
+  
+  // console.log(`[+] Contract Address: ${gameContract.address}`);
+  
+  // let txn;
+  
+  // for (let i=1; i <= 3; i++){
+  //   const shipId = randomShipNumber();
+  
+  //   txn = await gameContract.mintShipNFT(shipId);
+  //   await txn.wait();
     
-    console.log(`[+] Minted NFT #${i}`);
-  }
+  //   console.log(`[+] Minted NFT #${i}`);
+  // }
   
-  let tokenURI = await gameContract.tokenURI(1);
-  console.log(`[+] TokenURI: ${tokenURI}`);
+  // let tokenURI = await gameContract.tokenURI(1);
+  // console.log(`[+] TokenURI: ${tokenURI}`);
   
 }
 
-const randomShipNumber = () => {
-  const maxShips = 3;
+// const randomShipNumber = () => {
+//   const maxShips = 3;
   
-  return Math.floor(Math.random() * maxShips);
-}
+//   return Math.floor(Math.random() * maxShips);
+// }
 
 
 const runMain = async () => {
