ea3d05a7d1718e89932e0af6ad4e8a991b870636
mikeg1440 <gaudreaum01@gmail.com>
2022-07-05 17:25:18 -0400

    Update the ship image urls

--- scripts/utils/data.js
+++ scripts/utils/data.js
@@ -2,15 +2,15 @@
 const shipsData = [
   ["Rocinante", "Pella", "Donnager"],
   [
-    "https://static.wikia.nocookie.net/expanse/images/5/54/RociArtS4.png/revision/latest/scale-to-width-down/350?cb=20220113235011",
-    "https://static.wikia.nocookie.net/expanse/images/4/47/PellaConceptship.png/revision/latest/scale-to-width-down/350?cb=20201223193751",
-    "https://static.wikia.nocookie.net/expanse/images/8/8b/MCRN_Donnager_Render.jpg/revision/latest/top-crop/width/200/height/150?cb=20210614202656"
+    "https://static.wikia.nocookie.net/expanse/images/5/54/RociArtS4.png/revision/latest?cb=20220113235011",
+    "https://static.wikia.nocookie.net/expanse/images/4/47/PellaConceptship.png/revision/latest?cb=20201223193751",
+    "https://i.redd.it/w7l7271pbvy61.jpg"
   ],
-  [ 200, 300, 180 ],
+  [ 400, 600, 360 ],
   [ 100, 90, 105 ],
   'ProtoZoid',
   'https://i.pinimg.com/originals/b8/46/26/b846268133d5199992531d5a0885d398.jpg',
-  300,
+  900,
   120
 ]
 
