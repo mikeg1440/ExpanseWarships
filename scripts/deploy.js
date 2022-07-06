9c5201637abd14cee895119e4150772f9979bffb
mikeg1440 <gaudreaum01@gmail.com>
2022-07-06 12:54:09 -0400

    Update deploy script to test changes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,20 +7,20 @@ const main = async () => {
   await gameContract.deployed();
   console.log(`[+] Contract Address: ${gameContract.address}`);
   
-  let txn;
-  txn = await gameContract.mintShipNFT(2);
-  await txn.wait();
+  // let txn;
+  // txn = await gameContract.mintShipNFT(2);
+  // await txn.wait();
   
   // let returnedTokenUri = await gameContract.tokenURI(1);
   // console.log(`[+] Token URI: ${returnedTokenUri}`);
 
   console.log("[+] Contract deployed to ", gameContract.address);
   
-  txn = await gameContract.attackBoss();
-  let resp = await txn.wait();
+  // txn = await gameContract.attackBoss();
+  // let resp = await txn.wait();
   
-  txn = await gameContract.attackBoss();
-  resp = await txn.wait();
+  // txn = await gameContract.attackBoss();
+  // resp = await txn.wait();
   
   // await gameContract.deployed();
   
