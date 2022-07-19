1e8b0105d539bddb29939a093766b89936f069bb
mikeg1440 <gaudreaum01@gmail.com>
2022-07-18 22:26:20 -0400

    Create a component to select a warship

---
+++ warships-frontend/src/Components/SelectCharacter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function SelectCharacter({ setCharacterNFT }) {
+  return (
+    <div>Choose your ship wisely!</div>
+  )
+}
+
+const SelectContainer = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+`
