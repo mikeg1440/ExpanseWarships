06fe61130db9cd08d75c6f02569ce43970a1f837
mikeg1440 <gaudreaum01@gmail.com>
2022-07-18 22:53:26 -0400

    Add new method to transform the blockchain data to usable structure

--- warships-frontend/src/constants.js
+++ warships-frontend/src/constants.js
@@ -1,4 +1,12 @@
 
 const CONTRACT_ADDRESS = '0xC0508d61FdDe55AeB42657c1935805aF2fcFF879';
 
-export { CONTRACT_ADDRESS };
+const transformWarshipData = (warshipData) => {
+    name; warshipData.name,
+    imageURI: warshipData.imageURI,
+    hp: warshipData.hp.toNumber(),
+    maxHp: warshipData.maxHp.toNumber(),
+    attackDamage: warshipData.attackDamage.toNumber()
+};
+
+export { CONTRACT_ADDRESS,transformWarshipData };
