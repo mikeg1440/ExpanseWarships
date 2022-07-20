83474869be6f3d8e2526021313d2d7d64eea182c
mikeg1440 <gaudreaum01@gmail.com>
2022-07-19 20:47:56 -0400

    Fix error in return statement of transformWarshipData

--- warships-frontend/src/constants.js
+++ warships-frontend/src/constants.js
@@ -1,12 +1,13 @@
 
 const CONTRACT_ADDRESS = '0xC0508d61FdDe55AeB42657c1935805aF2fcFF879';
 
-const transformWarshipData = (warshipData) => {
-    name; warshipData.name,
+const transformWarshipData = (warshipData) => ({
+    name: warshipData.name,
     imageURI: warshipData.imageURI,
     hp: warshipData.hp.toNumber(),
     maxHp: warshipData.maxHp.toNumber(),
     attackDamage: warshipData.attackDamage.toNumber()
-};
+});
+
 
 export { CONTRACT_ADDRESS,transformWarshipData };
