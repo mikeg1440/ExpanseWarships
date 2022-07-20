3f654c8c932399714f67cc2497d8db324d462d89
mikeg1440 <gaudreaum01@gmail.com>
2022-07-19 20:52:00 -0400

    Remove old commented code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,46 +7,9 @@ const main = async () => {
   await gameContract.deployed();
   console.log(`[+] Contract Address: ${gameContract.address}`);
   
-  // let txn;
-  // txn = await gameContract.mintShipNFT(2);
-  // await txn.wait();
-  
-  // let returnedTokenUri = await gameContract.tokenURI(1);
-  // console.log(`[+] Token URI: ${returnedTokenUri}`);
-
   console.log("[+] Contract deployed to ", gameContract.address);
-  
-  // txn = await gameContract.attackBoss();
-  // let resp = await txn.wait();
-  
-  // txn = await gameContract.attackBoss();
-  // resp = await txn.wait();
-  
-  // await gameContract.deployed();
-  
-  // console.log(`[+] Contract Address: ${gameContract.address}`);
-  
-  // let txn;
-  
-  // for (let i=1; i <= 3; i++){
-  //   const shipId = randomShipNumber();
-  
-  //   txn = await gameContract.mintShipNFT(shipId);
-  //   await txn.wait();
-    
-  //   console.log(`[+] Minted NFT #${i}`);
-  // }
-  
-  // let tokenURI = await gameContract.tokenURI(1);
-  // console.log(`[+] TokenURI: ${tokenURI}`);
-  
 }
 
-// const randomShipNumber = () => {
-//   const maxShips = 3;
-  
-//   return Math.floor(Math.random() * maxShips);
-// }
 
 
 const runMain = async () => {
